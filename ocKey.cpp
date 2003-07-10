c29dcc8641d6b8284d7cdc77c457921eaaf95c93
asinghai <>
2003-07-10 19:50:14 +0000

    Some new features in Fit

--- ocKey.cpp
+++ ocKey.cpp
@@ -10,6 +10,7 @@
  #include <assert.h>
  #include <stdio.h>
  #include <stdlib.h>
+ #include <string.h>
  
  
 void ocKey::buildKey(ocKeySegment *key, int keysize, class ocVariableList *vars,
@@ -60,6 +61,20 @@ void ocKey::setKeyValue(ocKeySegment *key, int keysize, class ocVariableList *va
 	key[segment] = (key[segment] & ~mask) | ((value << var->shift) & mask);
 }
 
+/**	
+ * getKeyValue - get a single value in a key
+ */
+void ocKey::getKeyValue(ocKeySegment *key, int keysize, class ocVariableList *vars,
+		int index, int *value)
+{
+	int temp=0;
+	ocVariable *var = vars->getVariable(index);
+	int segment = var->segment;
+	ocKeySegment mask = var->mask;
+	temp = (key[segment] & mask); 
+ 	*value=temp >> var->shift;
+	assert(*value == DONT_CARE || (*value >= 0 && *value < var->cardinality));
+}
 
 /**
  * compareKeys - compare two keys
@@ -96,7 +111,27 @@ void ocKey::buildMask(ocKeySegment *mask, int keysize, class ocVariableList *var
 		ocKey::setKeyValue(mask, keysize, vars, var, 0);
 	}
 }
+/**
+ * buildMask - create a key with values as specified in states for included variables, 1's elsewhere
+ */
+/*
+void ocKey::buildMask(ocKeySegment *mask, int keysize, class ocVariableList *vars,
+		int *varindices, int varcount, int * states)
+{
+	//-- start with all DONT_CARE values
+	int i;
+	for (i = 0; i < keysize; i++) mask[i] = DONT_CARE;
+	
+	//-- now set zero's in the slots for this relation
 
+	int count = varcount;
+	int var;
+	for (i = 0; i < count; i++) {
+		var = varindices[i];	// var is index in variableList
+		ocKey::setKeyValue(mask, keysize, vars, var, 0);
+	}
+}
+*/
 /**
  * keyToString - generate printable form
  */
@@ -117,4 +152,45 @@ void ocKey::keyToString(ocKeySegment *key, ocVariableList *vars, char *str)
 	*cp = '\0';
 }
 
+void ocKey::keyToUserString(ocKeySegment *key, ocVariableList *vars, char *str)
+{
+	const char *valchars = "0123456789ABCDEF";
+	int i;
+	int varcount = vars->getVarCount();
+	char *cp = str;
+	for (i = 0; i < varcount; i++) {
+		ocVariable *var = vars->getVariable(i);
+		char **map =var->valmap;
+		ocKeySegment mask = var->mask;
+		int segment = var->segment;
+		int value = (key[segment] & mask) >> var->shift;
+		if(value<(var->cardinality)){
+			assert(value >= 0 && value < 16);
+			int len1=strlen(map[value]);
+                	strncpy(cp, map[value], len1);
+			cp += len1;
+		}
+	}
+	*cp = '\0';
+}
 
+void ocKey::getSibblings(ocKeySegment *key,ocVariableList *vars,ocTable *table,long *i_sibs,int DV_ind,int *no_sib)
+{
+	int keySize=vars->getKeySize();	
+	ocKeySegment *temp_key=new ocKeySegment[keySize];
+	memcpy((int *)temp_key,(int *)key,keySize*sizeof(long));	
+	int j=0;
+	ocVariable *var = vars->getVariable(DV_ind);
+        int card=var->cardinality;
+
+	setKeyValue(temp_key,keySize,vars,DV_ind,0);
+	for(int i=0;i<card;i++){
+		long index=table->indexOf(temp_key);
+		if(index>-1){
+			i_sibs[j++]=index;
+		}	
+		setKeyValue(temp_key,keySize,vars,DV_ind,i+1);
+	}
+	delete [] temp_key;
+	*no_sib=j;
+}
