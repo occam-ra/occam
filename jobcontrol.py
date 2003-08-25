cab38d5e2ca59f7b45b79bdaa178d4eff6d21d27
kwillett <>
2003-08-25 15:27:50 +0000

    ..

---
+++ jobcontrol.py
@@ -0,0 +1,33 @@
+import os, sys, cgi, sys, occam,time, string, traceback, pickle, re
+fieldIDs = [1,2,4,6,7]
+class JobControl:
+	def showJobs(self, formFields):
+		# Kill job if requested
+		pid = formFields.get("pid", 0)
+		if pid <> 0:
+			try:
+				os.kill(int(pid), 3)
+				print "<b>Job " + pid + " killed!</b><p>"
+			except Exception, e:
+				print "<b>", e.args[1], ": kill of ", pid, " failed</b><p>"
+			except:
+				print "<b>Kill of " + pid + " failed</b><p>"
+			
+
+		# Show active occam-related jobs
+		print "<b>Active Jobs</b><p>"
+		print "<table>"
+		procfd = os.popen("ps -f")
+		procstat = procfd.read()
+		procs = string.split(procstat, '\n');
+		for proc in procs:
+			if string.find(proc, "occam") >= 0:
+				print "<tr>"
+				fields = re.split("[ \t]+", proc, 7)
+				for fieldID in fieldIDs:
+					field = fields[fieldID]
+					print "<td>", field, "</td>"
+				print "<td><a href=\"weboccam.cgi?action=jobcontrol&pid=" + fields[1] + "\">kill</a></td>"
+				print "</tr>"
+		print "</table>"
+
