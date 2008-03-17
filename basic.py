6a76fe7e703ce9dd34bc863d97156578766bbcd4
jfusion <>
2008-03-17 23:45:07 +0000

    - cleaned up and expanded basic python script

--- basic.py
+++ basic.py
@@ -1,4 +1,5 @@
 #! /usr/bin/python
+import pdb
 import os, sys, occam
 
 import resource
@@ -6,110 +7,82 @@ resource.setrlimit(resource.RLIMIT_CORE, [360000, 360000])
 
 from ocutils import ocUtils
 
+if len(sys.argv) < 2:
+	print 'No data file specified.'
+	sys.exit()
+
+if len(sys.argv) >= 4:
+	swidth = sys.argv[2]
+	slevels = sys.argv[3]
+else:
+	swidth = 3;
+	slevels = 7;
+
+if len(sys.argv) >= 5:
+	filter = sys.argv[4]
+else:
+	filter = "loopless"
+
+util = ocUtils("VB")			# create a variable-based manager
+util.initFromCommandLine(sys.argv[0:2])	# initialize with the data file
+
 #------------- Main script ---------------
-# This script sets various options on a ocUtils object, and then
-# runs the desired action. ocUtils is a convenience wrapper around
-# the basic occam3 objects, and it sets appropriate defaults if you
-# don't set every option.
+# This script sets various options on a ocUtils object, and then runs the desired action.
+# ocUtils is a convenience wrapper around the basic occam3 objects, and it sets appropriate
+# defaults if you don't set every option.
 
-# For settings which require model attribute names,
-# the allowed values are:
-# h
-# t
-# df
-# ddf
-# loops		-- 1=loops, 0=no loops
-# information
-# unexplained
-# ipf_iterations
-# cond_h
-# cond_dh
-# cond_pct_dh
-# cond_df
-# cond_ddf
-# lr
-# alpha
-# beta
-# level
-# p2		-- note that the Pearson statistics aren't
-# p2		-- computed by default because they are expensive
-# p2_alpha
-# p2_alpha
-# p2_beta
-# p2_beta
+# For settings which require model attribute names, the allowed values include:
+# h, t, df, ddf, loops [1=loops, 0=no loops]
+# information, unexplained, ipf_iterations, lr, alpha, beta, level, aic, bic
+# cond_h, cond_dh, cond_pct_dh, cond_df, cond_ddf, p2, p2_alpha, p2_beta
+# (Note that the Pearson statistics aren't computed by default because they are expensive.)
+# To force a particular attribute to be printed as an integer, append "$I" to the name (e.g., Level$I)
 
-# To force a particular attribute to be printed as an integer,
-# append "$I" to the name (e.g., Level$I)
+# Set report names, from the list above. If omitted, the list is set based on whether the ref model
+# is top or bottom.  List is separated by commas, and provided as a single text string.
+# For ref=top, this is a good list:
+#util.setReportVariables("Level$I, h, ddf, lr, alpha, information, pct_correct_data, aic, bic")
+#util.setNoIPF(1)
+# For ref=bottom, use something like this:
+util.setReportVariables("level$I, h, ddf, lr, alpha, information, cond_pct_dh, aic, bic")
 
-#-------------------------------------------------
+# Set the start model for search [top, bottom, default, a specific model].
+# Skip this to use the model set in the data file.
+util.setStartModel("default")
+# Set the ref model [top, bottom, default, a specific model].
+util.setRefModel("default")
 
-# create an ocUtils object. This is a convenience wrapper which can
-# handle Occam2 files and options
-# See the source file for other options which can be set
-
-util = ocUtils("VB")
-util.initFromCommandLine(sys.argv)
-
-# set desired options which aren't in the data file, or override them
-# here.
-
-# set separator between report fields.
-# 1=tab, 2=comma, 3=space fill, 4=HTML
-util.setReportSeparator(3)
-
-# set report names, from the list above. If omitted, the list is set
-# based on whether the ref model is top or bottom. List is separated
-# by commas, and provided as a single text string
-# for ref=top, this is a good list:
-util.setReportVariables("Level$I, h, ddf, lr, alpha, information, pct_correct_data")
-util.setNoIPF(1)
-# for ref=bottom, use something like this:
-#util.setReportVariables("Level$I, h, ddf, lr, alpha, information")
-
-
-
-# set the model attribute on which sorting is done is done. This
-# controls the selection of "best models" during search. It can
-# also control reporting (see setReportSortName, below)
+# Set the search filter [all, loopless, disjoint, chain] and search direction [up, down].
+util.setSearchFilter(filter)
+#util.setSearchDir("up")
+# Set the model attribute on which sorting is done is done.  This controls the selection
+# of "best models" during search. It can also control reporting (see setReportSortName, below).
 util.setSortName("information")
+# Set the sorting direction for the search. ["ascending" prefers lower values, "descending" prefers higher]
+util.setSearchSortDir("descending")
+# Set the search width & number of levels.
+util.setSearchWidth(swidth)
+util.setSearchLevels(slevels)
 
-# set the model attribute for sorting the report, if it is different
-# from the attribute used during search. Generally this isn't needed.
-util.setReportSortName("information")
-
-# set the search direction (up, down)
-util.setSearchDir("up")
-
-# set the search filter (all, loopless, disjoint)
-util.setSearchFilter("all")
-
-# set the ref model (bottom, top, default, or a specific model)
-util.setRefModel("bottom")
-
-# set the start model for search (top, bottom, default, or a model name)
-# skip this to use the model set in the data file
-#util.setStartModel("ABC:CD")
-
-# set the sorting direction for pruning and reporting
-# ascending means lower values are better
-# descending means higher values are better
+# Set the model attribute for sorting the report, if it is different from the attribute used during search.
+# Generally this isn't needed.
+#util.setReportSortName("bic")
+# Set the sorting direction for reporting.
 util.setSortDir("descending")
 
-# set the search width
-util.setSearchWidth(25)
-
-# set the number of levels to search
-util.setSearchLevels(100)
-
-# set model for fit. Skip this to set it from the data file
+# Set model for fit.  Skip this to set it from the data file.
 #util.setFitModel("ABC:CD")
+# Set the action [fit, search].  Skip this to set it from the data file.
+util.setAction("search")
 
-# set the action (fit, search). Skip this to set it from the data file
-#util.setAction("fit")
+# Set separator between report fields.  [1=tab, 2=comma, 3=space fill, 4=HTML]
+util.setReportSeparator(3)
+# Perform the search or fit. Pass 1 as argument to print options, 0 not to.
+util.doAction(0)
 
-# leave this in, if you want a full list of program options printed
-util.printOptions("VB")
 
-# perform the search or fit, and print report
-util.doAction(1)
+
+
+
+
 
