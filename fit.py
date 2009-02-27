6169e611a569d7be342b5142168eec480620fe1b
jfusion <>
2009-02-27 08:16:03 +0000

    - fixed the fit.py script to do an actual Fit, the same way it is done by web Occam.

--- fit.py
+++ fit.py
@@ -1,31 +1,31 @@
-#! /usr/local/bin/python
-import os, sys, occam
-theModels = ( \
-	"A.B.C.D", \
-	"A.B.D:A.C.D:B.C.D" \
-)
+#! /usr/bin/python
+import pdb
+import os, sys
+sys.path.append("/www")
+import occam
+import time
 
-# process(model)
-# print info about the model
+from ocutils import ocUtils
 
-
-def process(model):
-	mgr.computeL2Statistics(model)
-	mgr.computePearsonStatistics(model)
-	mgr.computeDependentStatistics(model)
-	mgr.printFitReport(model)
-	return
-#end def
-		
+if len(sys.argv) < 3:
+	print 'Incorrect parameters.'
+	print 'Usage: %s datafile model' % sys.argv[0]
+	sys.exit()
 
 
-# BEGINNING of main script
+oc = ocUtils("VB")
 
-# create a variable based modeling manager, and initialize
-# it from the command line arguments (which contain the input file)
-mgr = occam.ocVBMManager()
-mgr.initFromCommandLine(sys.argv)
-for modelName in theModels:
-	model = mgr.makeModel(modelName, 1)
-	process(model)
+t1 = time.time()
+oc.initFromCommandLine(sys.argv[0:2]) # initialize with the data file
+
+oc.setFitModel(sys.argv[2])
+oc.setAction("fit")
+oc.setReportSeparator(3)
+
+t2 = time.time()
+oc.doAction(0)
+t3 = time.time()
+
+print "start:  %8f" % (t2 - t1)
+print "search: %8f" % (t3 - t2)
 
