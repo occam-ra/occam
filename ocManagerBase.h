f9cdcbd9c498274c0f389802d76d832785b7e7d0
jfusion <>
2009-11-13 01:21:33 +0000

    changed one function definition

--- ocManagerBase.h
+++ ocManagerBase.h
@@ -20,7 +20,7 @@
 
 class ocIntersectProcessor
 {
-public:
+    public:
 	ocIntersectProcessor() {}
 
 	//-- this function is called for each relation. sign is +1 for
@@ -40,24 +40,24 @@ public:
  * - calculation of various parameters for a table or model
  * - determination if a model contains loops
  */
- 
- class ocManagerBase {
- public:
- 	// method to use for computing H. Auto means use IPF if there are loops and
+
+class ocManagerBase {
+    public:
+	// method to use for computing H. Auto means use IPF if there are loops and
 	// algebraic method otherwise.
 	enum HMethod {AUTO, IPF, ALGEBRAIC};
-	
- 	// create an ocManagerBase object, supplying it with a variable list and a table
+
+	// create an ocManagerBase object, supplying it with a variable list and a table
 	// of input data.  Typically an application will read the input data and variable
 	// definitions, and then construct the appropriate manager.
 	ocManagerBase(ocVariableList *vars = 0, ocTable *input = 0);
-	
+
 	// initialize an ocManagerBase object, reading in standard options and data files.
 	bool initFromCommandLine(int argc, char **argv);
-	
+
 	// delete this object
 	virtual ~ocManagerBase();
-	
+
 	// get the relation defined by the list of variable indices and the count.
 	// this will first search the cache and, if the relation is found, return it.
 	// if not in the cache, a new relation is created. If instructed, the projection
@@ -67,25 +67,25 @@ public:
 	// get a relation just like the given relation, but with the given variable removed.
 	// (skip is the variable id in the master variable list).
 	virtual ocRelation *getChildRelation(ocRelation *rel, int skip, bool makeProject = false);
-	
- 	// make a projection.  This creates an ocTable, and installs it as the table for the
+
+	// make a projection.  This creates an ocTable, and installs it as the table for the
 	// given relation.  The projection is determined from the given table, and the
 	// VariableList is the one associated with the given relation. False is returned
 	// on any error. This function returns true immediately if the relation already
 	// has a table.
 	//if statebased the SB=0 else SB=1
- 	virtual bool makeProjection(ocRelation *rel,int SB=0);
+	virtual bool makeProjection(ocRelation *rel,int SB=0);
 
 	// make a projection of table t1 into t2 (empty), based on the variable
 	// list contained in the given relation. This is used as one step of the IPF algorithm.
 	virtual bool makeProjection(ocTable *t1, ocTable *t2, ocRelation *rel, int SB=0);
-	
+
 	// make a "maxProjection" of one table into another. This creates a partial
 	// probability distribution by keeping only the max values for each matching tuple.
 	// rel provides the set of variables to be used in the match (generally the IV set).
 	virtual bool makeMaxProjection(ocTable *t1, ocTable *t2, ocTable *inputData,
-				       ocRelation *indRel, ocRelation *depRel);
-		
+		ocRelation *indRel, ocRelation *depRel, double* missedValues);
+
 	// make projections for all relations in a model. This just calls makeProject
 	// as many times as needed.
 	virtual bool makeProjections(ocModel *model);
@@ -95,16 +95,16 @@ public:
 
 	// delete a model from the model cache
 	virtual bool deleteModelFromCache(ocModel *model);
-	
+
 	// Make a fit table. This function uses the IPF algorithm. The fit table is
 	// linked to the model.  If the model already has a fit table, the function
 	// returns immediately. False is returned on any error.
 	virtual bool makeFitTable(ocModel *model,int SB=0);
-	
+
 	// Expand a single tuple into all values of all missing variables, recursively
 	void expandTuple(double tupleValue, ocKeySegment *key,
-			 int *missingVars, int missingCount,
-			 ocTable *outTable, int currentMissingVar);
+		int *missingVars, int missingCount,
+		ocTable *outTable, int currentMissingVar);
 
 	void makeOrthoExpansion(ocRelation *rel, ocTable *table);
 
@@ -116,10 +116,10 @@ public:
 	// plus the second order overlaps, etc. This also computes entropy, though
 	// it isn't correct if the model contains loops. For models with loops, use IPF.
 	void DFAndEntropy(ocModel *model);
-	
+
 	// Determine if the model has loops, and cache this fact for later use
 	bool hasLoops(ocModel *model);
-	
+
 	// compute various relation and model attributes. False is returned on error
 	// (e.g., no table has been computed) If computation is successful, the
 	// attribute is added to the attribute list for the relation or model.
@@ -138,7 +138,7 @@ public:
 	virtual ocModel *getRefModel() { return refModel; };
 	virtual int getSearchDirection() = 0;
 	virtual int getUseInverseNotation() { return useInverseNotation; };
-	
+
 	int getKeySize() { return keysize; }
 	double getSampleSz() { return sampleSize; }
 	double getTestSampleSize() { return testSampleSize; }
@@ -158,19 +158,19 @@ public:
 	//-- ":" as relation separator. Optionally the data projection can be created.
 	ocModel *makeModel(const char *name, bool makeProject = true);
 	ocModel *makeSBModel(const char *name, bool makeProject = true);
-	
+
 	//-- get and set options. The options are initialized by command line or datafile options
 	bool setOptionString(ocOptionDef *def, const char *value)
-		{ return options->setOptionString(def, value); }
+	{ return options->setOptionString(def, value); }
 	bool setOptionFloat(ocOptionDef *def, double nvalue)
-		{ return options->setOptionFloat(def, nvalue); }
+	{ return options->setOptionFloat(def, nvalue); }
 	bool getOptionString(const char *name, void **next, const char **value)
-		{ return options->getOptionString(name, next, value); }
+	{ return options->getOptionString(name, next, value); }
 	bool getOptionFloat(const char *name, void **next, double *nvalue)
-		{ return options->getOptionFloat(name, next, nvalue); }
+	{ return options->getOptionFloat(name, next, nvalue); }
 
 	void printOptions(bool printHTML);
-	
+
 	class ocTable *getFitTable() { return fitTable1; }
 	//state based Model functions
 	//calculates the number of state constarints generated 
@@ -180,14 +180,14 @@ public:
 	//add the state constraints for a relation 
 	int addConstraint(int varcount,int *varindices,int *stateindices,int* stateindices_c,ocKeySegment* start,ocRelation *rel);
 	void make_SS(int statespace=0);
-	
+
 	//-- Print debug info on memory usage
 	void printSizes();
 
 	//-- print out all relations
 	void dumpRelations();
 
-protected:
+    protected:
 	ocModel *topRef;
 	ocModel *bottomRef;
 	ocModel *refModel;
@@ -202,7 +202,7 @@ protected:
 	class ocRelCache *relCache;
 	class ocModelCache *modelCache;
 	class ocOptions *options;
- 	ocTable *fitTable1;
+	ocTable *fitTable1;
 	ocTable *fitTable2;
 	ocTable *projTable;
 	int **State_Space_Arr;
@@ -210,8 +210,8 @@ protected:
 	int *DVOrder;
 	int useInverseNotation;
 };
- 
- #endif
- 
+
+#endif
+
 
 
