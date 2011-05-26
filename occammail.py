482296787ee80f6c21743e7df51458585c4a26ae
jfusion <>
2011-05-26 23:36:37 +0000

    changes to support python email module (previously used modules are being deprecated)

--- occammail.py
+++ occammail.py
@@ -6,42 +6,40 @@
 # 2 filename
 # 3 optional subject line addendum
 
-import sys, os, string, smtplib, mimetools, MimeWriter, cStringIO
-import socket
+import sys, os, smtplib, socket
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 def sendMessage(toaddr, msg):
-	server = smtplib.SMTP('mailhost.pdx.edu')
-	server.sendmail('occam-feedback@lists.pdx.edu', toaddr, msg)
-	server.quit()
+    server = smtplib.SMTP('mailhost.pdx.edu')
+    msg['From'] = 'occam-feedback@lists.pdx.edu'
+    msg['To'] = toaddr
+    server.sendmail('occam-feedback@lists.pdx.edu', toaddr, msg.as_string())
+    server.quit()
 
-def buildMessage(infile, filename):
-	outfd = cStringIO.StringIO()
-	writer = MimeWriter.MimeWriter(outfd)
-	if emailSubject != "":
-	    writer.addheader('Subject', 'Occam Results: ' + emailSubject)
-	else:
-	    writer.addheader('Subject', 'Occam Results: ' + filename)
-	writer.flushheaders();
-	writer.startmultipartbody('mixed')
-	writer.flushheaders()
-	subpart = writer.nextpart()
-	pout = subpart.startbody('text/plain')
-	pout.write('Occam result file ' + filename + ' is attached.\n')
-	pout.write('Sent from server ' + socket.gethostname() + '.\n')
-	subpart = writer.nextpart()
-	pout = subpart.startbody('application/octet-stream', [('name', filename)])
-	line = infile.readline()
-	while line:
-		pout.write(line)
-		line = infile.readline()
-	writer.lastpart()
-	msg = outfd.getvalue()
-	outfd.close()
-	return msg
+def buildMessage(infile, filename, emailSubject):
+    msg = MIMEMultipart()
+    if emailSubject != "":
+        msg['Subject'] = 'OCCAM Results: ' + emailSubject
+    else:
+        msg['Subject'] = 'OCCAM Results: ' + filename
+    email = 'OCCAM result file ' + filename + ' is attached.\n'
+    email += 'Sent from server ' + socket.getfqdn() + '.\n'
+    emailmsg = MIMEText(email, 'plain')
+    msg.attach(emailmsg)
+    line = infile.readline()
+    csv = line
+    while line:
+        csv += line
+        line = infile.readline()
+    csvmsg = MIMEText(csv, _subtype='csv')
+    csvmsg.add_header('Content-Disposition', 'attachment', filename=filename)
+    msg.attach(csvmsg)
+    return msg
 
 toaddr = sys.argv[1]
 filename = sys.argv[2]
 emailSubject = sys.argv[3].decode("hex")
-msg = buildMessage(sys.stdin, filename)
+msg = buildMessage(sys.stdin, filename, emailSubject)
 sendMessage(toaddr, msg)
 
