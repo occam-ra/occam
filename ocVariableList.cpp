f89cfffffffa9cf59d74d3128d43d8520fdea788
jfusion <>
2011-05-26 23:35:43 +0000

    changes to support SB model names

--- ocVariableList.cpp
+++ ocVariableList.cpp
@@ -6,7 +6,7 @@
  * variables in a problem, including name, cardinality, and the position of the variable
  * value in the tuple key.
  */
- 
+
 #include "ocCore.h"
 #include "_ocCore.h"
 #include <assert.h>
@@ -19,9 +19,9 @@ static const char *findUpper(const char *cp)
 {
     //-- find next upper case character, indicating beginning of a variable
     for(;;) {
-	char ch = *(++cp);	// go to next one first
-	if (ch == '\0') return 0;
-	if (isupper(ch)) break;
+        char ch = *(++cp);	// go to next one first
+        if (ch == '\0') return 0;
+        if (isupper(ch)) break;
     }
     return cp;
 }
@@ -31,7 +31,7 @@ static void normalizeCase(char *cp)
     //-- convert first letter to upper case, rest lower
     if (islower(*cp)) *cp = toupper(*cp);
     while (*(++cp)) {
-	if (isupper(*cp)) *cp = tolower(*cp);
+        if (isupper(*cp)) *cp = tolower(*cp);
     }
 }
 
@@ -51,17 +51,17 @@ ocVariableList::ocVariableList(int maxVars)
     noUseMaskSize = maxVars;
     noUseMask = new bool[noUseMaskSize];
     for (int i = 0; i < noUseMaskSize; i++)
-	noUseMask[i] = false;
+        noUseMask[i] = false;
 }
 
 
 ocVariableList::~ocVariableList()
 {
     for (int i = 0; i < varCount; i++) {
-	ocVariable *varp = vars + i;
-	for (int j = 0; j < varp->cardinality; j++) {
-	    delete varp->valmap[j];
-	}
+        ocVariable *varp = vars + i;
+        for (int j = 0; j < varp->cardinality; j++) {
+            delete varp->valmap[j];
+        }
     }
     if (vars) delete vars;
 }
@@ -80,14 +80,14 @@ int ocVariableList::addVariable(const char *name, const char *abbrev, int cardin
 {
     const int GROWTH_FACTOR = 2;
     while (varCount >= maxVarCount) {
-	vars = (ocVariable*) growStorage(vars, maxVarCount*sizeof(ocVariable), GROWTH_FACTOR);
-	maxVarCount *= GROWTH_FACTOR;
+        vars = (ocVariable*) growStorage(vars, maxVarCount*sizeof(ocVariable), GROWTH_FACTOR);
+        maxVarCount *= GROWTH_FACTOR;
     }
     ocVariable *varp = vars + (varCount++);
     varCountDF++;
     while (varCountDF > noUseMaskSize) {
-	noUseMask = (bool*) growStorage(noUseMask, noUseMaskSize*sizeof(bool), GROWTH_FACTOR);
-	noUseMaskSize *= GROWTH_FACTOR;
+        noUseMask = (bool*) growStorage(noUseMask, noUseMaskSize*sizeof(bool), GROWTH_FACTOR);
+        noUseMaskSize *= GROWTH_FACTOR;
     }
     strncpy(varp->name, name, MAXNAMELEN); varp->name[MAXNAMELEN] = '\0';
     strncpy(varp->abbrev, abbrev, MAXABBREVLEN); varp->abbrev[MAXABBREVLEN] = '\0';
@@ -106,25 +106,25 @@ int ocVariableList::addVariable(const char *name, const char *abbrev, int cardin
     //-- 0..cardinality (to include the DONT_CARE value)
     int size = 0;
     while (cardinality != 0) {	// log2 computation for small numbers
-	size++;
-	cardinality >>= 1;
+        size++;
+        cardinality >>= 1;
     }
     varp->size = size;
 
     //-- for first variable, treat as special case
     if (varp == vars) {
-	varp->segment = 0;
-	varp->shift = KEY_SEGMENT_BITS - varp->size;
+        varp->segment = 0;
+        varp->shift = KEY_SEGMENT_BITS - varp->size;
     } else {
-	//-- see if key will fit in the last segment of the key
-	ocVariable *lastp = varp - 1;	// previous entry
-	if (lastp->shift >= varp->size) {
-	    varp->segment = lastp->segment;
-	    varp->shift = lastp->shift - varp->size;
-	} else {	// add a new segment
-	    varp->segment = lastp->segment + 1;
-	    varp->shift = KEY_SEGMENT_BITS - varp->size;
-	}
+        //-- see if key will fit in the last segment of the key
+        ocVariable *lastp = varp - 1;	// previous entry
+        if (lastp->shift >= varp->size) {
+            varp->segment = lastp->segment;
+            varp->shift = lastp->shift - varp->size;
+        } else {	// add a new segment
+            varp->segment = lastp->segment + 1;
+            varp->shift = KEY_SEGMENT_BITS - varp->size;
+        }
     }
     // build a mask with bits in the variable positions
     ocKeySegment keytemp = 1;
@@ -157,8 +157,8 @@ int ocVariableList::markForNoUse()
     //grow the mask if the variables are more than the mask can hold
     const int GROWTH_FACTOR = 2;
     while (varCountDF >= noUseMaskSize) {
-	noUseMask = (bool*) growStorage(noUseMask, noUseMaskSize*sizeof(bool), GROWTH_FACTOR);
-	noUseMaskSize *= GROWTH_FACTOR;
+        noUseMask = (bool*) growStorage(noUseMask, noUseMaskSize*sizeof(bool), GROWTH_FACTOR);
+        noUseMaskSize *= GROWTH_FACTOR;
     }
     noUseMask[varCountDF] = true;
     varCountDF++;
@@ -173,31 +173,31 @@ int ocVariableList::getNewValue(int index,char * old_value,char*new_value){
     int chr;
     char myvalue[100];
     for (chr = 0; chr < 100; chr++) {
-	if (old_value[chr] == '\0' || isspace(old_value[chr]) || old_value[chr] == ',') break;
-	else myvalue[chr] = old_value[chr];
+        if (old_value[chr] == '\0' || isspace(old_value[chr]) || old_value[chr] == ',') break;
+        else myvalue[chr] = old_value[chr];
     }
     myvalue[chr]='\0';
     //exclude case
     if((vars+index)->exclude != NULL){
-	if((k=strcmp(myvalue,(vars+index)->exclude))==0)return -1;
-	else {
-	    strcpy(new_value,myvalue);
-	    return 1;
-	}
+        if((k=strcmp(myvalue,(vars+index)->exclude))==0)return -1;
+        else {
+            strcpy(new_value,myvalue);
+            return 1;
+        }
     }
     for(;;){
-	char * value_new=(vars+index)->oldnew[1][i];
-	char * value_old=(vars[index]).oldnew[0][i];
-	if(value_new==NULL)break;
-	if((k=strcmp(value_old,myvalue))==0){
-	    strcpy(new_value,value_new);
-	    return 1;
-	}
-	else if((k=strcmp(value_old,"*"))==0){
-	    strcpy(new_value,value_new);
-	    return 1;
-	}
-	i++;
+        char * value_new=(vars+index)->oldnew[1][i];
+        char * value_old=(vars[index]).oldnew[0][i];
+        if(value_new==NULL)break;
+        if((k=strcmp(value_old,myvalue))==0){
+            strcpy(new_value,value_new);
+            return 1;
+        }
+        else if((k=strcmp(value_old,"*"))==0){
+            strcpy(new_value,value_new);
+            return 1;
+        }
+        i++;
     }
     return -1;
 }
@@ -231,16 +231,16 @@ void ocVariableList::dump()
     printf("ocVariableList: varCount = %d, maxVarCount=%d\n", varCount, maxVarCount);
     printf("\tDV\tCard\tSeg\tSiz\tShft\tName\t\tAbb\tMask\n");
     for (int i = 0; i < varCount; i++) {
-	ocVariable *v = vars + i;
-	printf("\t%d\t%d\t%d\t%d\t%d\t%8s\t%s\t%0*lx\t", v->dv, v->cardinality, v->segment, v->size, v->shift, v->name, v->abbrev, sizeof(ocKeySegment)*2, v->mask);
-	for(int j=0; j < v->cardinality; j++) {
-	    printf("%s\t", v->valmap[j]);
-	}
-	printf("\n");
+        ocVariable *v = vars + i;
+        printf("\t%d\t%d\t%d\t%d\t%d\t%8s\t%s\t%0*lx\t", v->dv, v->cardinality, v->segment, v->size, v->shift, v->name, v->abbrev, sizeof(ocKeySegment)*2, v->mask);
+        for(int j=0; j < v->cardinality; j++) {
+            printf("%s\t", v->valmap[j]);
+        }
+        printf("\n");
     }
     printf("No-Use Mask:");
     for (int i = 0; i < noUseMaskSize; i++)
-	noUseMask[i] ? printf("1") : printf("0");
+        noUseMask[i] ? printf("1") : printf("0");
     printf("\n");
 }
 
@@ -254,31 +254,30 @@ void ocVariableList::getPrintName(char *str, int maxlength, int count, int *vars
     char *cp = str;
     maxlength--;
     for (i = 0; i < count; i++) {
-	if (maxlength <= 0) break;	// no room
-	int varID = vars1[i];
-	char *name = getVariable(varID)->abbrev;
-	int len = strlen(name);
-	strncpy(cp, name, maxlength);
-	maxlength -= len;
-	cp += len;
-	if(states != 0) {
-	    int stateID = 0;
-	    // left here needs modification
-	    stateID = states[i];
-	    if(stateID != DONT_CARE){
-		char **map = vars[varID].valmap;
-		int len1 = strlen(map[stateID]);
-		strncpy(cp, map[stateID], maxlength);
-		maxlength -= len1;
-		cp += len1;
-	    }
-
-	}
-	if (i < count-1) { // at least one more to go
-	    if (maxlength <= 0) break;
-	    //*cp++ = '.';
-	    maxlength--;
-	}
+        if (maxlength <= 0) break;	// no room
+        int varID = vars1[i];
+        char *name = getVariable(varID)->abbrev;
+        int len = strlen(name);
+        strncpy(cp, name, maxlength);
+        maxlength -= len;
+        cp += len;
+        if(states != NULL) {
+            int stateID = 0;
+            // left here needs modification
+            stateID = states[i];
+            if(stateID != DONT_CARE) {
+                char **map = vars[varID].valmap;
+                int len1 = strlen(map[stateID]);
+                strncpy(cp, map[stateID], maxlength);
+                maxlength -= len1;
+                cp += len1;
+            }
+        }
+        if (i < count-1) { // at least one more to go
+            if (maxlength <= 0) break;
+            //*cp++ = '.';
+            maxlength--;
+        }
     }
     assert(maxlength >= 0);
     *cp = '\0';
@@ -288,19 +287,23 @@ void ocVariableList::getPrintName(char *str, int maxlength, int count, int *vars
 /**
  * Determine length of printed name. This is conservative but not exact.
  */
-int ocVariableList::getPrintLength(int count, int *vars, bool state_b)
+int ocVariableList::getPrintLength(int count, int *vars, int *states)
 {
     int i;
     int len = 0;
+    int varID, stateID, slen;
+    char *name;
     for (i = 0; i < count; i++) {
-	int varID = vars[i];
-	char *name = getVariable(varID)->abbrev;
-	int card = getVariable(varID)->cardinality;
-	//not consistent...state id and state value are to different things
-	card = card / 10;
-	len += strlen(name);
-	len=len+card+1;
-	//len += strlen(name) + 1; // an extra for separator character
+        varID = vars[i];
+        name = getVariable(varID)->abbrev;
+        len += strlen(name);
+        if (states) {
+            stateID = states[i];
+            if(stateID != DONT_CARE) {
+                slen = strlen(getVariable(varID)->valmap[stateID]);
+                len += slen;
+            }
+        }
     }
     return len;
 }
@@ -313,7 +316,7 @@ int ocVariableList::getPrintLength(int count, int *vars, bool state_b)
 bool ocVariableList::isDirected()
 {
     for (int i = varCount - 1; i >= 0; i--) {
-	if (vars[i].dv) return true;
+        if (vars[i].dv) return true;
     }
     return false;
 }
@@ -322,7 +325,7 @@ bool ocVariableList::isDirected()
 int ocVariableList::getDV()
 {
     for (int i = varCount - 1; i >= 0; i--) {
-	if (vars[i].dv) return i;
+        if (vars[i].dv) return i;
     }
     return -1;
 }
@@ -338,26 +341,26 @@ int ocVariableList::getVariableList(const char *name, int *varlist)
     char vname[MAXNAMELEN];
     int pos = 0;
     while (cp != NULL) {
-	int i, len;
-	cp2 = findUpper(cp);
-	if (cp2 != NULL) {	// not the last variable
-	    len = cp2 - cp;
-	    strncpy(vname, cp, len);
-	    vname[len] = '\0';
-	    cp = cp2;
-	} else {	// last variable
-	    strcpy(vname, cp);
-	    cp = NULL;
-	}
-	for (i = 0; i < varCount; i++) {
-	    if (strcasecmp(vars[i].abbrev, vname) == 0) break;
-	}
-	if (i >= varCount) {
-	    fprintf(stderr, "variable %s not found\n", vname);
-	    return 0;	// bad name
-	} else {
-	    varlist[pos++] = i;
-	}
+        int i, len;
+        cp2 = findUpper(cp);
+        if (cp2 != NULL) {	// not the last variable
+            len = cp2 - cp;
+            strncpy(vname, cp, len);
+            vname[len] = '\0';
+            cp = cp2;
+        } else {	// last variable
+            strcpy(vname, cp);
+            cp = NULL;
+        }
+        for (i = 0; i < varCount; i++) {
+            if (strcasecmp(vars[i].abbrev, vname) == 0) break;
+        }
+        if (i >= varCount) {
+            fprintf(stderr, "variable %s not found\n", vname);
+            return 0;	// bad name
+        } else {
+            varlist[pos++] = i;
+        }
     }
     return pos;
 }
@@ -373,45 +376,45 @@ int ocVariableList::getVarStateList(const char *name, int *varlist,int *stlist)
     int value=0;
     int pos = 0;
     while (cp != NULL) {
-	int i=0, len,j;
-	cp2 = findUpper(cp);
-	if (cp2 != NULL) {	// not the last variable
-	    len = cp2 - cp;
-	    strncpy(vname, cp, len);
-	    vname[len] = '\0';
-	    j=sscanf(vname, "%[A-Z,a-z]%[0-9,.]",vname1,sname);
-	    /*if(j==2){printf("vname is %s and vanme 1 is %s and sname is %s\n",vname,vname1,sname);
-	      }
-	      else if(j==1)printf("vname1 is %s\n",vname1);*/
-	    cp = cp2;
-	}
-	else {	// last variable
-	    strcpy(vname, cp);
-	    j=sscanf(vname,"%[a-z,A-Z]%[0-9,.]",vname1,sname);
-	    //printf("vname is %s\n",vname);
-	    //printf("vname1 is %s\n",vname1);
-	    cp = NULL;
-	}
-	//printf("i is %d\n and varcount is %d",i,varCount);
-	for (i = 0; i < varCount; i++) {
-	    if (strcasecmp(vars[i].abbrev, vname1) == 0) break;
-	}
-	if (i >= varCount) {
-	    fprintf(stderr, "variable %s not found\n", vname);
-	    return 0;	// bad name
-	}
-	else {
-	    //printf("I is %d\n",i);
-	    varlist[pos] = i;
-	    if(j==2){
-		value = getVarValueIndex(i,sname);
-		//printf("value for sname %s is %d\n",sname,value);
-		stlist[pos]=value;
-	    }else{
-		stlist[pos]=DONT_CARE; 
-	    }
-	    pos++;
-	}
+        int i=0, len,j;
+        cp2 = findUpper(cp);
+        if (cp2 != NULL) {	// not the last variable
+            len = cp2 - cp;
+            strncpy(vname, cp, len);
+            vname[len] = '\0';
+            j=sscanf(vname, "%[A-Z,a-z]%[0-9,.]",vname1,sname);
+            /*if(j==2){printf("vname is %s and vanme 1 is %s and sname is %s\n",vname,vname1,sname);
+              }
+              else if(j==1)printf("vname1 is %s\n",vname1);*/
+            cp = cp2;
+        }
+        else {	// last variable
+            strcpy(vname, cp);
+            j=sscanf(vname,"%[a-z,A-Z]%[0-9,.]",vname1,sname);
+            //printf("vname is %s\n",vname);
+            //printf("vname1 is %s\n",vname1);
+            cp = NULL;
+        }
+        //printf("i is %d\n and varcount is %d",i,varCount);
+        for (i = 0; i < varCount; i++) {
+            if (strcasecmp(vars[i].abbrev, vname1) == 0) break;
+        }
+        if (i >= varCount) {
+            fprintf(stderr, "variable %s not found\n", vname);
+            return 0;	// bad name
+        }
+        else {
+            //printf("I is %d\n",i);
+            varlist[pos] = i;
+            if(j==2){
+                value = getVarValueIndex(i,sname);
+                //printf("value for sname %s is %d\n",sname,value);
+                stlist[pos]=value;
+            }else{
+                stlist[pos]=DONT_CARE; 
+            }
+            pos++;
+        }
     }
     return pos;
 }
@@ -427,25 +430,25 @@ int ocVariableList::getVarValueIndex(int varindex, const char *value)
 
     //-- extract the name as a separate string
     for (chr = 0; chr < 100; chr++) {
-	if (value[chr] == '\0' || isspace(value[chr]) || (value[chr] == ',')) break;
-	else myvalue[chr] = value[chr];
+        if (value[chr] == '\0' || isspace(value[chr]) || (value[chr] == ',')) break;
+        else myvalue[chr] = value[chr];
     }
     myvalue[chr] = '\0';
 
     //-- find this value in the value map. Stop on first empty entry
     while (index < cardinality) {
-	if (map[index] == NULL) break;
-	if (strcmp(myvalue, map[index]) == 0) return index;
-	index++;
+        if (map[index] == NULL) break;
+        if (strcmp(myvalue, map[index]) == 0) return index;
+        index++;
     }
     //-- if we have room, add this value. Otherwise return error.
     if (index < cardinality) {
-	map[index] = new char[strlen(value)+1];
-	strcpy(map[index], myvalue);
-	return index;
+        map[index] = new char[strlen(value)+1];
+        strcpy(map[index], myvalue);
+        return index;
     }
     else
-	return -1;
+        return -1;
 }
 
 
@@ -462,15 +465,19 @@ bool ocVariableList::checkCardinalities()
 {
     bool result = true;
     for (int varindex = 0; varindex < varCount; varindex++) {
-	int cardinality =vars[varindex].cardinality;
-	char **map = vars[varindex].valmap;
-	int valuecount = 0;
-	while (map[valuecount] != NULL) valuecount++;
-	if (valuecount != cardinality) {
-	    printf("Warning: input data cardinality for variable %s is %d, which is less than the specified value of %d.\n",
-		    vars[varindex].name, valuecount, cardinality);
-	    // result = false; // no longer fatal
-	}
+        int cardinality =vars[varindex].cardinality;
+        char **map = vars[varindex].valmap;
+        int valuecount = 0;
+        while (map[valuecount] != NULL) valuecount++;
+        if (valuecount < cardinality) {
+            printf("Warning: input data cardinality for variable %s (%s) is %d, but was specified as %d. The lower value will be used.\n",
+                    vars[varindex].name, vars[varindex].abbrev, valuecount, cardinality);
+            vars[varindex].cardinality = valuecount;
+        } else if (valuecount > cardinality) {
+            printf("ERROR: input data cardinality for variable %s (%s) is %d, but was specified as %d. OCCAM will quit.\n",
+                    vars[varindex].name, vars[varindex].abbrev, valuecount, cardinality);
+            result = false;
+        }
     }
     return result;
 }
