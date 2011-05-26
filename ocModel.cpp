305034e8fa908513b749317537d12a7daa7fadc3
jfusion <>
2011-05-26 23:28:12 +0000

    whitespace only

--- ocModel.cpp
+++ ocModel.cpp
@@ -52,7 +52,7 @@ long ocModel::size()
 
 bool ocModel::isStateBased() {
     for (int i = 0; i < relationCount; i++) {
-	if (relations[0]->isStateBased()) return true;
+        if (relations[0]->isStateBased()) return true;
     }
     return false;
 }
@@ -69,8 +69,8 @@ void ocModel::copyRelations(ocModel &model, int skip1, int skip2)
 {
     int count = model.getRelationCount();
     for (int i = 0; i < count; i++) {
-	ocRelation *rel = model.getRelation(i);
-	if (skip1 != i && skip2 != i) addRelation(rel, false);
+        ocRelation *rel = model.getRelation(i);
+        if (skip1 != i && skip2 != i) addRelation(rel, false);
     }
 }
 
@@ -94,42 +94,42 @@ int * ocModel::get_indicesfromKey(ocKeySegment *key, ocVariableList *vars, int s
     //maximum number of states that can constrained by a relation
     int *indices = new int[statespace];
     for (int i = 0; i < varcount; i++) {
-	ocVariable *var = vars->getVariable(i);
-	ocKeySegment mask = var->mask;
-	int segment = var->segment;
-	int value = 0;
-	//printf("segment is %d and key is %x\n",segment,key[segment]);
-	ocKeySegment mask1;
-	//printf("mask1 is %x and key is %x\n",mask,key[segment]);
-	if((mask1=(mask & key[segment]))==mask) value_l[i] = DONT_CARE;
-	else{
-	    value = (key[segment] & mask) >> var->shift;
-	    value_l[i] = value;
-	}
-	//printf("value for variable %d is %d\n",i,value_l[i]);
+        ocVariable *var = vars->getVariable(i);
+        ocKeySegment mask = var->mask;
+        int segment = var->segment;
+        int value = 0;
+        //printf("segment is %d and key is %x\n",segment,key[segment]);
+        ocKeySegment mask1;
+        //printf("mask1 is %x and key is %x\n",mask,key[segment]);
+        if((mask1=(mask & key[segment]))==mask) value_l[i] = DONT_CARE;
+        else{
+            value = (key[segment] & mask) >> var->shift;
+            value_l[i] = value;
+        }
+        //printf("value for variable %d is %d\n",i,value_l[i]);
     }	
     *counter=0;	
     int not_a_match = -1;
     for (int i=0; i < statespace; i++) {
-	for (int j=0; j < varcount; j++) {
-	    if (value_l[j] == DONT_CARE) continue;
-	    //printf("state space value %d and value_l %d\n",stateSpaceArr[i][j],value_l[j]);
-	    if (value_l[j] != stateSpaceArr[i][j]) {
-		not_a_match = 1;
-		break;
-	    } else {
-		not_a_match = 0;
-	    }
-	}
-	//printf("value for match %d\n",not_a_match);
-	if (not_a_match == 0) {
-	    //printf("counter value is %d\n",*counter);
-	    indices[(*counter)] = i;
-	    //printf("indice is %d\n",indices[(*counter)]);
-	    (*counter)++;
+        for (int j=0; j < varcount; j++) {
+            if (value_l[j] == DONT_CARE) continue;
+            //printf("state space value %d and value_l %d\n",stateSpaceArr[i][j],value_l[j]);
+            if (value_l[j] != stateSpaceArr[i][j]) {
+                not_a_match = 1;
+                break;
+            } else {
+                not_a_match = 0;
+            }
+        }
+        //printf("value for match %d\n",not_a_match);
+        if (not_a_match == 0) {
+            //printf("counter value is %d\n",*counter);
+            indices[(*counter)] = i;
+            //printf("indice is %d\n",indices[(*counter)]);
+            (*counter)++;
 
-	}
-	not_a_match = -1;
+        }
+        not_a_match = -1;
 
     }
     return indices;
@@ -143,9 +143,9 @@ void ocModel::makeStructMatrix(int statespace, ocVariableList *vars, int **state
     int const_count = 0;
     int i = 0;
     for (i = 0; i < count; i++) {
-	ocRelation *rel = getRelation(i);
-	ocStateConstraint *sc = rel->getStateConstraints();
-	const_count += sc->getConstraintCount();
+        ocRelation *rel = getRelation(i);
+        ocStateConstraint *sc = rel->getStateConstraints();
+        const_count += sc->getConstraintCount();
     }
     //printf("total no of constraints in the model %d\n",const_count);
     Total_constraints = const_count+1;
@@ -160,54 +160,54 @@ void ocModel::makeStructMatrix(int statespace, ocVariableList *vars, int **state
      */
     /* NEW CODE added */
     for(i=0; i <= const_count; i++){
-	stateSpaceArr1 = new int[statespace];
-	structMatrix[i] = stateSpaceArr1;
+        stateSpaceArr1 = new int[statespace];
+        structMatrix[i] = stateSpaceArr1;
     }
     //printf("count is: %d\n", count);
     //printf("stateSpace is: %d\n", statespace);
     //exit(1);
     for(i=0; i < statespace; i++){
-	for(int j=0; j < const_count; j++){
-	    structMatrix[j][i] = 0;
-	}
-	//the default constraint
-	structMatrix[const_count][i] = 1;
+        for(int j=0; j < const_count; j++){
+            structMatrix[j][i] = 0;
+        }
+        //the default constraint
+        structMatrix[const_count][i] = 1;
     }
     const_count = 0;
     int T_const_count = 0;
     for (i = 0; i < count; i++) {
-	ocRelation *rel = getRelation(i);
-	if(rel == NULL){
-	    printf("error happened in file : ocModel.cpp after getRelation\n");
-	    exit(1);
-	}
-	ocStateConstraint *sc = rel->getStateConstraints();
-	if(sc == NULL){
-	    printf("error happened in file : ocModel.cpp after getStateConstraints\n");
-	    exit(1);
-	}
-	const_count = sc->getConstraintCount();
-	if(const_count <= 0){
-	    printf("error happened in file : ocModel.cpp after getConstraintCount\n");
-	    exit(1);
-	}
-	for(int j=0; j < const_count; j++){
-	    ocKeySegment* key = sc->getConstraint(j);	
-	    if(key == NULL)
-	    {
-		printf("error happened in file : ocModel.cpp after getConstraint\n");
-		exit(1);
-	    }
-	    int counter;
-	    int *indices = get_indicesfromKey(key, vars, statespace, stateSpaceArr, &counter);			
-	    //printf("number of 1s in the constraint %d\n",counter);
-	    for(int k=0; k < counter; k++){	
-		structMatrix[T_const_count+j][indices[k]] = 1;
-	    }
-	    //printf("row for %d constraint in %d relation\n",j,i);
+        ocRelation *rel = getRelation(i);
+        if(rel == NULL){
+            printf("error happened in file : ocModel.cpp after getRelation\n");
+            exit(1);
+        }
+        ocStateConstraint *sc = rel->getStateConstraints();
+        if(sc == NULL){
+            printf("error happened in file : ocModel.cpp after getStateConstraints\n");
+            exit(1);
+        }
+        const_count = sc->getConstraintCount();
+        if(const_count <= 0){
+            printf("error happened in file : ocModel.cpp after getConstraintCount\n");
+            exit(1);
+        }
+        for(int j=0; j < const_count; j++){
+            ocKeySegment* key = sc->getConstraint(j);	
+            if(key == NULL)
+            {
+                printf("error happened in file : ocModel.cpp after getConstraint\n");
+                exit(1);
+            }
+            int counter;
+            int *indices = get_indicesfromKey(key, vars, statespace, stateSpaceArr, &counter);			
+            //printf("number of 1s in the constraint %d\n",counter);
+            for(int k=0; k < counter; k++){	
+                structMatrix[T_const_count+j][indices[k]] = 1;
+            }
+            //printf("row for %d constraint in %d relation\n",j,i);
 
-	}
-	T_const_count += const_count;
+        }
+        T_const_count += const_count;
     }
 }
 
@@ -222,44 +222,44 @@ void ocModel::addRelation(ocRelation *relation, bool normalize)
 
     //-- if normalize, compare this relation to existing relations
     if (normalize) {
-	for (i = 0; i < relationCount; i++) {
-	    if (relations[i]->contains(relation)) {
-		// don't need this one. Assuming caching we don't explicitly delete it since it
-		// may be referenced elsewhere.
-		return;
-	    }
-	    else if (relation->contains(relations[i])) {
-		// remove this relation from model
-		for (j = i; j < relationCount-1; j++) {
-		    relations[j] = relations[j+1];
-		}
-		relationCount--;
-		i--; // fix up loop counter since we deleted one
-	    }
-	}
-	//-- grow storage if needed
-	while (relationCount >= maxRelationCount) {
-	    relations = (ocRelation**) growStorage(relations, maxRelationCount*sizeof(ocRelation*), FACTOR);
-	    maxRelationCount *= FACTOR;
-	}
-	//-- now find the spot for this relation and add it in
-	for (i = 0; i < relationCount; i++) {
-	    if (relation->compare(relations[i]) < 0) break;
-	}
-	for(j = relationCount; j > i; j--) {
-	    relations[j] = relations[j-1];
-	}
-	relations[i] = relation;
-	relationCount++;
+        for (i = 0; i < relationCount; i++) {
+            if (relations[i]->contains(relation)) {
+                // don't need this one. Assuming caching we don't explicitly delete it since it
+                // may be referenced elsewhere.
+                return;
+            }
+            else if (relation->contains(relations[i])) {
+                // remove this relation from model
+                for (j = i; j < relationCount-1; j++) {
+                    relations[j] = relations[j+1];
+                }
+                relationCount--;
+                i--; // fix up loop counter since we deleted one
+            }
+        }
+        //-- grow storage if needed
+        while (relationCount >= maxRelationCount) {
+            relations = (ocRelation**) growStorage(relations, maxRelationCount*sizeof(ocRelation*), FACTOR);
+            maxRelationCount *= FACTOR;
+        }
+        //-- now find the spot for this relation and add it in
+        for (i = 0; i < relationCount; i++) {
+            if (relation->compare(relations[i]) < 0) break;
+        }
+        for(j = relationCount; j > i; j--) {
+            relations[j] = relations[j-1];
+        }
+        relations[i] = relation;
+        relationCount++;
     }
     else {
-	// if we know a priori the relations are normalized and in order, we can skip all this stuff.
+        // if we know a priori the relations are normalized and in order, we can skip all this stuff.
 
-	while (relationCount >= maxRelationCount) {
-	    relations = (ocRelation**) growStorage(relations, maxRelationCount*sizeof(ocRelation*), FACTOR);
-	    maxRelationCount *= FACTOR;
-	}
-	relations[relationCount++] = relation;
+        while (relationCount >= maxRelationCount) {
+            relations = (ocRelation**) growStorage(relations, maxRelationCount*sizeof(ocRelation*), FACTOR);
+            maxRelationCount *= FACTOR;
+        }
+        relations[relationCount++] = relation;
     }
 }
 
@@ -281,9 +281,9 @@ ocRelation *ocModel::getRelation(int index)
 // Returns true if this model contains the specified relation; false otherwise.
 bool ocModel::containsRelation(ocRelation *relation) {
     for (int i = relationCount -1; i >= 0; --i) {
-	//if (relation->compare(relations[i]) == 0)
-	if (relations[i]->contains(relation))
-	    return true;
+        //if (relation->compare(relations[i]) == 0)
+        if (relations[i]->contains(relation))
+            return true;
     }
     return false;
 }
@@ -291,9 +291,9 @@ bool ocModel::containsRelation(ocRelation *relation) {
 
 bool ocModel::containsModel(ocModel *child) {
     for (int i = 0; i < child->getRelationCount(); i++) {
-	if (!this->containsRelation(child->getRelation(i))) {
-	    return false;
-	}
+        if (!this->containsRelation(child->getRelation(i))) {
+            return false;
+        }
     }
 }
 
@@ -319,8 +319,8 @@ void ocModel::setFitTable(ocTable *tbl)
 void ocModel::deleteFitTable()
 {
     if (fitTable) {
-	delete fitTable;
-	fitTable = NULL;
+        delete fitTable;
+        fitTable = NULL;
     }
 }
 
@@ -332,39 +332,39 @@ const char * ocModel::getPrintName(int useInverse)
     bool isDirected = relations[0]->getVariableList()->isDirected();
     int indOnlyRel = -1;
     if ( (printName == NULL && useInverse == 0) || (inverseName == NULL && useInverse == 1) ) {
-	for (i = 0; i < relationCount; i++) {
-	    const char *name;
-	    //-- for directed systems, figure out which relation is the indOnly one
-	    if (isDirected && relations[i]->isIndependentOnly()) {
-		indOnlyRel = i;
-		name = "IV";
-	    } else {
-		name  = relations[i]->getPrintName(useInverse);
-	    }
-	    len += strlen(name) + 1;
-	}
-	if (len < 40) len = 40;	//??String allocation bug?
-	char *tempName = new char[len+1];
-	char *cp = tempName;
-	*cp = '\0';
+        for (i = 0; i < relationCount; i++) {
+            const char *name;
+            //-- for directed systems, figure out which relation is the indOnly one
+            if (isDirected && relations[i]->isIndependentOnly()) {
+                indOnlyRel = i;
+                name = "IV";
+            } else {
+                name  = relations[i]->getPrintName(useInverse);
+            }
+            len += strlen(name) + 1;
+        }
+        if (len < 40) len = 40;	//??String allocation bug?
+        char *tempName = new char[len+1];
+        char *cp = tempName;
+        *cp = '\0';
 
-	//-- format the name. For directed systems put the string IV: first, 
-	//-- and skip the independent only relation
-	if (indOnlyRel >= 0) {
-	    strcpy(cp, "IV");
-	    cp += strlen(cp);
-	}
-	for (i = 0; i < relationCount; i++) {	
-	    if (i == indOnlyRel) continue;
-	    // if the string isn't empty, put in a colon separator
-	    if (cp != tempName) *(cp++) = ':';
-	    name = relations[i]->getPrintName(useInverse);
-	    strcpy(cp, name);
-	    cp += strlen(name);
-	}
-	*cp = '\0';
-	if (useInverse == 0) printName = tempName;
-	else inverseName = tempName;
+        //-- format the name. For directed systems put the string IV: first, 
+        //-- and skip the independent only relation
+        if (indOnlyRel >= 0) {
+            strcpy(cp, "IV");
+            cp += strlen(cp);
+        }
+        for (i = 0; i < relationCount; i++) {	
+            if (i == indOnlyRel) continue;
+            // if the string isn't empty, put in a colon separator
+            if (cp != tempName) *(cp++) = ':';
+            name = relations[i]->getPrintName(useInverse);
+            strcpy(cp, name);
+            cp += strlen(name);
+        }
+        *cp = '\0';
+        if (useInverse == 0) printName = tempName;
+        else inverseName = tempName;
     }
     if (useInverse == 0) return printName;
     else return inverseName;
@@ -376,10 +376,10 @@ void ocModel::printStructMatrix(){
     int Total_const;
     int **str_matrix = get_structMatrix(&statespace, &Total_const);
     for (int i=0; i < Total_const; i++) {
-	for(int j=0; j < statespace; j++) {
-	    printf("%d,", str_matrix[i][j]);
-	}	
-	printf("\n");
+        for(int j=0; j < statespace; j++) {
+            printf("%d,", str_matrix[i][j]);
+        }	
+        printf("\n");
     }
 }	
 
@@ -391,11 +391,11 @@ void ocModel::dump(bool detail)
     printf("\n");
     printf("\t\tSize: %d,\tRelCount: %d,\tMaxRel:%d\n", size(), getRelationCount(), maxRelationCount);
     if (detail) {
-	if (fitTable)
-	    printf(",\tFitTable: %d", fitTable->size());
-	for (int i = 0; i < relationCount; i++) {
-	    relations[i]->dump();
-	}
+        if (fitTable)
+            printf(",\tFitTable: %d", fitTable->size());
+        for (int i = 0; i < relationCount; i++) {
+            relations[i]->dump();
+        }
     }
     printf("\n");
 
