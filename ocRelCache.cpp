2abad8429e46921da17d083b53a3dc5ef9363c7a
jfusion <>
2012-04-30 23:39:56 +0000

    Fixed relCache to work with relation names rather than variable masks, so SB relations can be cached.

--- ocRelCache.cpp
+++ ocRelCache.cpp
@@ -7,107 +7,95 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <memory.h>
+#include <string.h>
 
-static int hashcode(ocKeySegment *key, int keysize, int hashsize)
-{
-    //-- compute a hashcode, an integer from 0..hashsize-1, for the key.
-    //-- this is done by XOR of the key segments, and then modulus
-    ocKeySegment k = 0;
-    for (int i = 0; i < keysize; i++) k ^= key[i];
-    return k % hashsize;
+static int hashcode(const char *name, int hashsize) {
+    unsigned int key = 0;
+    for (const char *cp = name; *cp; cp++)
+        key = (key << 1) + *cp;
+    return key % hashsize;
 }
 
-
-ocRelCache::ocRelCache()
-{
+ocRelCache::ocRelCache() {
     hash = new ocRelation*[RELCACHE_HASHSIZE];
-    memset(hash, 0, RELCACHE_HASHSIZE*sizeof(ocRelation*));
+    memset(hash, 0, RELCACHE_HASHSIZE * sizeof(ocRelation*));
 }
-
 
 //-- destroy relation cache.  This also deletes all the relations held in the cache.
-ocRelCache::~ocRelCache()
-{
+ocRelCache::~ocRelCache() {
     ocRelation *r1, *r2;
     int i;
     for (i = 0; i < RELCACHE_HASHSIZE; i++) {
-	r1 = hash[i];
-	while (r1) {
-	    r2 = r1->getHashNext();
-	    delete r1;
-	    r1 = r2;
-	}
+        r1 = hash[i];
+        while (r1) {
+            r2 = r1->getHashNext();
+            delete r1;
+            r1 = r2;
+        }
     }
     delete hash;
 }
 
-
-long ocRelCache::size()
-{
+long ocRelCache::size() {
     long size = RELCACHE_HASHSIZE * sizeof(ocRelation*);
     ocRelation *r1;
     int i;
     for (i = 0; i < RELCACHE_HASHSIZE; i++) {
-	r1 = hash[i];
-	while (r1) {
-	    size += r1->size();
-	    r1 = r1->getHashNext();
-	}
+        r1 = hash[i];
+        while (r1) {
+            size += r1->size();
+            r1 = r1->getHashNext();
+        }
     }
     return size;
 }
 
-
 //-- delete tables from all relations
-void ocRelCache::deleteTables()
-{
+void ocRelCache::deleteTables() {
     ocRelation *r1;
     int i;
     for (i = 0; i < RELCACHE_HASHSIZE; i++) {
-	r1 = hash[i];
-	while (r1) {
-	    r1->deleteTable();
-	    r1 = r1->getHashNext();
-	}
+        r1 = hash[i];
+        while (r1) {
+            r1->deleteTable();
+            r1 = r1->getHashNext();
+        }
     }
 }
-
 
 //-- addRelation - put a new relation in the cache. If a matching relation already
 //-- exists, an error is returned.
 // [JSF] This doesn't seem to check for matches, or return errors.
-bool ocRelCache::addRelation(class ocRelation *rel)
-{
+bool ocRelCache::addRelation(class ocRelation *rel) {
+    if (findRelation(rel->getPrintName()) != NULL)
+        return false; //error; exists
     ocKeySegment *mask = rel->getMask();
-    int hashindex = hashcode(mask, rel->getKeySize(), RELCACHE_HASHSIZE);
+    int hashindex = hashcode(rel->getPrintName(), RELCACHE_HASHSIZE);
     rel->setHashNext(hash[hashindex]);
     hash[hashindex] = rel;
     return true;
 }
 
-
 //-- findRelation - find a relation in the cache.  Null is returned if the given
 //-- relation doesn't exist.
-class ocRelation *ocRelCache::findRelation(ocKeySegment *mask, int keysize)
-{
-    int hashindex = hashcode(mask, keysize, RELCACHE_HASHSIZE);
+class ocRelation *ocRelCache::findRelation(const char *name) {
+    int hashindex = hashcode(name, RELCACHE_HASHSIZE);
     ocRelation *rp = hash[hashindex];
-    while (rp && ocKey::compareKeys(rp->getMask(), mask, keysize) != 0) rp = rp->getHashNext();
-    return rp;	// either NULL, or the matching one
+    while (rp && strcmp(name, rp->getPrintName()) != 0)
+        rp = rp->getHashNext();
+    return rp; // either NULL, or the matching one
 }
 
-
 //-- dump - print out all relations in the cache
-void ocRelCache::dump()
-{
+void ocRelCache::dump() {
     printf("\nDumping RelCache:\n");
     for (int i = 0; i < RELCACHE_HASHSIZE; i++) {
-	if (hash[i]) {
-	    printf ("hash chain [%d]:\n", i);
-	    for (ocRelation *rel = hash[i]; rel; rel = rel->getHashNext()) {
-		rel->dump();
-	    }
-	}
+        if (hash[i]) {
+            printf("hash chain [%d]:\n", i);
+            for (ocRelation *rel = hash[i]; rel; rel = rel->getHashNext()) {
+                rel->dump();
+            }
+        }
     }
 }
 
