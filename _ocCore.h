2e84bb929f1c9fae153d6e05589ad745456ea74d
jfusion <>
2012-04-30 22:51:25 +0000

    Added qsort_r macro to handle different versions on different architectures.

--- _ocCore.h
+++ _ocCore.h
@@ -7,6 +7,23 @@
 #include "stdlib.h"
 #include "stdio.h"
 
+/*#if NEED_DECLARE_QSORT_R
+//// NOTE: this declaration must match os-features-test.c .
+void qsort_r(void *base, size_t nmemb, size_t sz,
+             void *userdata,
+             int (*compar)(void *, const void *, const void *));
+#endif*/
+
+#if SWAP_QSORT_R
+/* macosx style */
+#define QSORT_R qsort_r
+#define QSORT_COMPARISON_FUNCTION(func, thunk, v1, v2) func(thunk, v1, v2)
+#else
+#define QSORT_R(a,b,c,d,e) qsort_r(a,b,c,e,d)
+#define QSORT_COMPARISON_FUNCTION(func, thunk, v1, v2) func(v1, v2, thunk)
+#endif
+
+
 #define fmax(a, b) ((a) > (b) ? (a) : (b))
 
 #define growStorage(old, osize, factor) (_growStorage((old), (unsigned long long) (osize), (factor), __FILE__, __LINE__))
@@ -27,6 +44,7 @@ int ocCompareVariables(int varCount1, int *var1, int varCount2, int *var2);
  * Determine if one variable list contains another
  */
 bool ocContainsVariables(int varCount1, int *var1, int varCount2, int *var2);
+bool ocContainsStates(int var_count1, int *var1, int *states1, int var_count2, int *var2, int *states2);
 
 #undef TRACE_MEMORY
 
