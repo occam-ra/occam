6f00dbf9ef8d7dc3c574007c7cbfca36eee881fc
jfusion <>
2012-11-14 23:21:25 +0000

    Trying to fix job control.

--- weboccam.py
+++ weboccam.py
@@ -519,7 +519,8 @@ def getFormFields(form):
 def getUniqueFilename(file_name):
     dirname, filename = os.path.split(file_name)
     prefix, suffix = os.path.splitext(filename)
-    '_'.join(prefix.split())
+    print prefix, suffix
+    prefix = '_'.join(prefix.split())
     fd, filename = tempfile.mkstemp(suffix, prefix+"__", dirname)
     os.chmod(filename, 0660)
     return filename
