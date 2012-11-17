72e8c579aad29f1e5e41d811ffc2b8242e3b682d
jfusion <>
2012-11-17 03:58:40 +0000

    Version number.

--- weboccam.py
+++ weboccam.py
@@ -10,7 +10,7 @@ from jobcontrol import JobControl
 #import urllib2
 #import platform, traceback
 
-VERSION = "3.3.9"
+VERSION = "3.3.10"
 
 false = 0; true = 1
 # perhaps we should do some check that this directory exists?
