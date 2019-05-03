e102b9c28c06866703c2979ce5f26e2c82951981
crubio <rcarla@pdx.edu>
2019-05-03 16:56:50 -0700

    added model methods

--- py/occampy3/wrappers/model.py
+++ py/occampy3/wrappers/model.py
@@ -16,39 +16,58 @@ class Model:
         """
         :param: ref: the reference to the Model object returned from the CPP engine
         """
+        # Create new reference if one not given
         self._ref = ref
         self._id = 0
-
-    @property
-    def ref(self):
-        return self._ref
-
-    @property
-    def print_name(self) -> str:
-        return self._ref.getPrintName()
+        self._progenitor = 0
 
     @property
     def id_(self) -> int:
         return self._id
 
     @id_.setter
-    def id_(self, id_):
+    def id_(self, id_: int) -> None:
         self._id = id_
 
     @property
-    def fit_table(self):
+    def fit_table(self) -> None:
         pass
 
     @fit_table.setter
-    def fit_table(self, model):
+    def fit_table(self, model) -> None:
         self._ref.makeFitTable(model)
 
     @fit_table.deleter
     def fit_table(self):
         self._ref.deleteFitTable()
 
+    def set(self, **kwargs):
+        pass
+
+    @property
+    def print_name(self) -> str:
+        return self._ref.getPrintName()
+
     def get_struct_matrix(self) -> list:
         return self._ref.getStructMatrix()
 
-    def set(self, **kwargs):
-        pass
+    def get_ref(self):
+        return self._ref
+
+    def is_equivalent_to(self) -> bool:
+        return bool(self._ref.isEquivalentTo())
+
+    def get_progenitor(self) -> Model:
+        return self._ref.getProgenitor()
+
+    def set_progenitor(self) -> None:
+        self._progenitor = self._ref.setProgenitor()
+
+    def get_relation(self) -> Relation:
+        return self._ref.getRelation()
+
+    def delete_relation_links(self):
+        self._ref.deleteRelationLinks()
+
+    def dump(self, model) -> Model:
+        return self._ref.dump()
