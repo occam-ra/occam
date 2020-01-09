8049faecbae8fdc93ed71637853f86880e9956b1
Shawn Marincas <marincas@pdx.edu>
2020-01-09 06:40:39 +0000

    Add mostRecentRun.txt to .gitignore and fix install script

--- install_occam.sh
+++ install_occam.sh
@@ -8,13 +8,15 @@ sudo apt upgrade
 sudo apt install -y apache2
 sudo apt install -y python2.7 python-pip
 sudo apt install -y gcc build-essential libgmp3-dev python-dev libxml2 libxml2-dev zlib1g-dev
+pip install python-igraph
 
 # Make OCCAM
 make install
 
 # Setup Apache
 sudo a2enmod cgi
-echo <<EOT >> /etc/apache2/sites-enabled/000-default.conf
+#cat <<EOT >> /etc/apache2/sites-enabled/000-default.conf
+sudo tee -a /etc/apache2/sites-enabled/000-default.conf > /dev/null <<EOT
 <VirtualHost *:80>
 	# The ServerName directive sets the request scheme, hostname and port that
 	# the server uses to identify itself. This is used when creating
@@ -56,12 +58,62 @@ echo <<EOT >> /etc/apache2/sites-enabled/000-default.conf
 EOT
 
 # TODO Change /etc/apache2/envvars
+#cat <<EOT >> /etc/apache2/envvars
+sudo tee -a /etc/apache2/envvars > /dev/null <<EOT
+# envvars - default environment variables for apache2ctl
+
+# this won't be correct after changing uid
+unset HOME
+
+# for supporting multiple apache2 instances
+if [ "${APACHE_CONFDIR##/etc/apache2-}" != "${APACHE_CONFDIR}" ] ; then
+	SUFFIX="-${APACHE_CONFDIR##/etc/apache2-}"
+else
+	SUFFIX=
+fi
+
+# Since there is no sane way to get the parsed apache2 config in scripts, some
+# settings are defined via environment variables and then used in apache2ctl,
+# /etc/init.d/apache2, /etc/logrotate.d/apache2, etc.
+export APACHE_RUN_USER=occam
+export APACHE_RUN_GROUP=occam
+# temporary state file location. This might be changed to /run in Wheezy+1
+export APACHE_PID_FILE=/var/run/apache2$SUFFIX/apache2.pid
+export APACHE_RUN_DIR=/var/run/apache2$SUFFIX
+export APACHE_LOCK_DIR=/var/lock/apache2$SUFFIX
+# Only /var/log/apache2 is handled by /etc/logrotate.d/apache2.
+export APACHE_LOG_DIR=/var/log/apache2$SUFFIX
+
+## The locale used by some modules like mod_dav
+export LANG=C
+## Uncomment the following line to use the system default locale instead:
+#. /etc/default/locale
+
+export LANG
+
+## The command to get the status for 'apache2ctl status'.
+## Some packages providing 'www-browser' need '--dump' instead of '-dump'.
+#export APACHE_LYNX='www-browser -dump'
+
+## If you need a higher file descriptor limit, uncomment and adjust the
+## following line (default is 8192):
+#APACHE_ULIMIT_MAX_FILES='ulimit -n 65536'
+
+## If you would like to pass arguments to the web server, add them below
+## to the APACHE_ARGUMENTS environment.
+#export APACHE_ARGUMENTS=''
+
+## Enable the debug mode for maintainer scripts.
+## This will produce a verbose output on package installations of web server modules and web application
+## installations which interact with Apache
+#export APACHE2_MAINTSCRIPT_DEBUG=1
+EOT
 
 # Set Permissions
-chown -R www-data install/web/
-chgrp -R www-data install/web/
+chown -R occam install/web/
+chgrp -R occam install/web/
 chmod -R 750 install/web/
 chmod g+s install/web/
 chmod g+w install/web/data/
 
-# 
+sudo systemctl restart apache2
