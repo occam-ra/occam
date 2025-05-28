e1935772ef1996cd0de9529ca9977ba2702fb2c7
Bart Massey <bart.massey@gmail.com>
2025-05-28 14:26:16 -0700

    added explicit name to podman container run

--- podman/run.sh
+++ podman/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-podman run -d -p 8080:80 occam_local && echo "You can now connect at http://localhost:8080/"
+podman run -d -p 8080:80 --name occam_local occam_local && echo "You can now connect at http://localhost:8080/"
