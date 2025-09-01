8d2fe2a1f217a82dcc56a1a4b9adb5665dcc87f0
Percy <percyd@pdx.edu>
2025-09-01 16:07:10 -0700

    Fix setup.py EOF error and platform-specific linking

--- pyoccam/setup.py
+++ pyoccam/setup.py
@@ -25,4 +25,30 @@ ext_modules = [
             os.path.join(cpp_dir, file) for file in [
                 'AttributeList.cpp', 'Input.cpp', 'Key.cpp', 'ManagerBase.cpp',
                 'ManagerInitFromCommandLine.cpp', 'Model.cpp', 'ModelCache.cpp',
-                'OccamMath.cpp', 'Options.c
+                'OccamMath.cpp', 'Options.cpp', 'RelCache.cpp', 'Relation.cpp',
+                'Report.cpp', 'ReportCommon.cpp', 'ReportPrintConditionalDV.cpp',
+                'ReportPrintResiduals.cpp', 'ReportQsort.cpp', 'SBMManager.cpp',
+                'Search.cpp', 'SearchBase.cpp', 'StateConstraint.cpp', 'Table.cpp',
+                'VBMManager.cpp', 'VariableList.cpp', '_Core.cpp'
+            ]
+        ] + [os.path.join(here, 'pyoccam_pybind11.cpp')],
+        include_dirs=[
+            pybind11.get_include(),
+            include_dir,
+            cpp_dir
+        ],
+        language='c++',
+        extra_compile_args=extra_compile_args,
+        extra_link_args=extra_link_args,
+    ),
+]
+
+setup(
+    name='pyoccam',
+    version='0.1.0',
+    author='David Percy',
+    description='OCCAM Reconstructability Analysis Tools - Python binding',
+    ext_modules=ext_modules,
+    cmdclass={'build_ext': build_ext},
+    zip_safe=False,
+)
